eabff3122cb5f653fc12eb3e3d9c8131144090ac
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-11-09 15:02:32 +0900

    feat: configure env variables

--- apps/freshTour-batch/src/fresh-tour-batch.module.ts
+++ apps/freshTour-batch/src/fresh-tour-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FreshTourBatchController } from './fresh-tour-batch.controller';
 import { FreshTourBatchService } from './fresh-tour-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [FreshTourBatchController],
   providers: [FreshTourBatchService],
 })
--- apps/freshTour-batch/src/main.ts
+++ apps/freshTour-batch/src/main.ts
@@ -3,6 +3,6 @@ import { FreshTourBatchModule } from './fresh-tour-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(FreshTourBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
