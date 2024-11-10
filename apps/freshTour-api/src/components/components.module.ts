3ef9c68c63963bf1ce1ed92af8e0f6a31507d1f6
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-11-10 11:56:27 +0900

    feat: create components related modules

--- apps/freshTour-api/src/components/components.module.ts
+++ apps/freshTour-api/src/components/components.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
-import { PropertyModule } from './property/property.module';
+import { HotelModule } from './hotel/hotel.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule]
+	imports: [
+		MemberModule,
+		AuthModule,
+		HotelModule,
+		BookingModule,
+		BoardArticleModule,
+		CommentModule,
+		LikeModule,
+		ViewModule,
+		FollowModule,
+	],
 })
 export class ComponentsModule {}
