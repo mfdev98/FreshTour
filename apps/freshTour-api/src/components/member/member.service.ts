5db291ee7ef8aa3a17d78e0d51f32cc846ba6180
MUKHAMMADYUSUF <mfdev98@gmail.com>
2025-01-11 19:42:11 +0900

    feat: develop signup business logic

--- apps/freshTour-api/src/components/member/member.service.ts
+++ apps/freshTour-api/src/components/member/member.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
-	public async signup(): Promise<string> {
-		return 'signup executed!';
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+
+	public async signup(input: MemberInput): Promise<Member> {
+		//todo Hash password
+
+		try {
+			const result = await this.memberModel.create(input);
+			//todo Authentication
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+		}
 	}
 
 	public async login(): Promise<string> {
