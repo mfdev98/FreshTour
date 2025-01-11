d31ac09e10f5fba31ec83417be8f14790c7611d2
MUKHAMMADYUSUF <mfdev98@gmail.com>
2025-01-11 19:56:49 +0900

    feat: develop login business logic

--- apps/freshTour-api/src/components/member/member.service.ts
+++ apps/freshTour-api/src/components/member/member.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
-import { MemberInput } from '../../libs/dto/member/member.input';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Message } from '../../libs/enums/common.enum';
+import { MemberStatus } from '../../libs/enums/member.enum';
 
 @Injectable()
 export class MemberService {
@@ -22,8 +23,23 @@ export class MemberService {
 		}
 	}
 
-	public async login(): Promise<string> {
-		return 'login executed!';
+	public async login(input: LoginInput): Promise<Member> {
+		const { memberNick, memberPassword } = input;
+		const response: Member = await this.memberModel
+			.findOne({ memberNick: memberNick })
+			.select('+memberPassword')
+			.exec();
+		console.log('response:', response);
+		if (!response || response.memberStatus === MemberStatus.DELETE) {
+			throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+		} else if (response.memberStatus === MemberStatus.BLOCK) {
+			throw new InternalServerErrorException(Message.BLOCKED_USER);
+		}
+
+		//todo Compare passwords
+		const isMatch = memberPassword === response.memberPassword;
+		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+		return response;
 	}
 
 	public async updateMember(): Promise<string> {
